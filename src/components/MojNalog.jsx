d3d59c80d14a326e54859f285d544be1d4eed2d5
AndrejaKrstic <andrejakrstic5121@gmail.com>
2022-01-30 17:47:45 +0100

    uradjena promena lozinke

--- src/components/MojNalog.jsx
+++ src/components/MojNalog.jsx
@@ -3,6 +3,9 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
 
 const style = {
   color: "white",
@@ -19,6 +22,12 @@ const style = {
 };
 
 function MojNalog() {
+  let navigate = useNavigate();
+  const [passwordData, setPasswordData] = useState({
+    oldPassword: "",
+    newPassword: "",
+  });
+
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
@@ -31,7 +40,60 @@ function MojNalog() {
       document.getElementById("div-promena-lozinke").style.display = "block";
     }
   }
-  function handleAccountDelete() {}
+  function handleInput(e) {
+    let newPasswordData = passwordData;
+    newPasswordData[e.target.name] = e.target.value;
+    setPasswordData(newPasswordData);
+    console.log(passwordData);
+  }
+  function handlePasswordChange(e) {
+    e.preventDefault();
+    var config = {
+      method: "post",
+      url: "users/password",
+      headers: {
+        Authorization: "Bearer " + window.sessionStorage.getItem("auth_token"),
+      },
+      data: passwordData,
+    };
+    console.log(config);
+    axios(config).then((res) => {
+      console.log(res.data);
+      if (res.data == "password succesfully changed!") {
+        document.getElementById("txt-uspeh-promene").style.color = "green";
+        document.getElementById("txt-uspeh-promene").style.display = "block";
+        document.getElementById("txt-uspeh-promene").innerHTML =
+          "Uspešno promenjena lozinka!";
+        document.getElementById("div-promena-lozinke").style.display = "none";
+        setTimeout(function () {
+          document.getElementById("txt-uspeh-promene").style.display = "none";
+        }, 3000);
+      } else if (res.data == "old password is not correct!") {
+        document.getElementById("txt-uspeh-promene").style.color = "red";
+        document.getElementById("txt-uspeh-promene").style.display = "block";
+        document.getElementById("txt-uspeh-promene").innerHTML =
+          "Pogrešna stara lozinka!";
+        setTimeout(function () {
+          document.getElementById("txt-uspeh-promene").style.display = "none";
+        }, 3000);
+      }
+    });
+  }
+  function handleAccountDelete() {
+    var config = {
+      method: "delete",
+      url: "users/" + window.sessionStorage.getItem("id"),
+      headers: {
+        Authorization: "Bearer " + window.sessionStorage.getItem("auth_token"),
+      },
+    };
+    axios(config).then(() => {
+      window.sessionStorage.setItem("auth_token", null);
+      window.sessionStorage.setItem("imePrezime", null);
+      window.sessionStorage.setItem("id", null);
+      navigate("/login");
+    });
+  }
   return (
     <>
       <div className="div-moj-nalog">
@@ -40,16 +102,25 @@ function MojNalog() {
           Promenite lozinku
         </button>
         <div className="div-promena-lozinke" id="div-promena-lozinke">
-          <form action="submit">
-            <input type="password" className="input-password-old" />
+          <form action="submit" onSubmit={handlePasswordChange}>
+            <input
+              type="password"
+              className="input-password-old"
+              onInput={handleInput}
+              name="oldPassword"
+            />
             <p>Unesite staru lozinku</p>
-            <input type="password" className="input-password-new" />
+            <input
+              type="password"
+              className="input-password-new"
+              onInput={handleInput}
+              name="newPassword"
+            />
             <p>Unesite novu lozinku</p>
-            <input type="password" className="input-password-new-repeat" />
-            <p>Ponovite novu lozinku</p>
             <button className="btn-promena-lozinke-action">Promeni</button>
           </form>
         </div>
+        <p className="txt-uspeh-promene" id="txt-uspeh-promene"></p>
         <br />
         <button className="btn-brisanje-naloga" onClick={handleOpen}>
           Obrišite nalog
